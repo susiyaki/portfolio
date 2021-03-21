75b4a566468a815e38939e84fe4471f387ebf3bb
susiyaki <susiyaki.dev@gmail.com>
2021-03-21 16:48:50 +0900

    add firestore

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,15 +1,22 @@
 import firebase from "firebase/app";
+import "firebase/firestore";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCDZfL4gBogSrPIMgpTwAclQhTixMyNf8o",
-  authDomain: "susiyaki-portfolio.firebaseapp.com",
-  projectId: "susiyaki-portfolio",
-  storageBucket: "susiyaki-portfolio.appspot.com",
-  messagingSenderId: "52419541464",
-  appId: "1:52419541464:web:5f4fab7f0f74106a98daad",
-  measurementId: "G-Z3EXZ0CJMS",
-};
+let db: firebase.firestore.Firestore;
 
-firebase.initializeApp(firebaseConfig);
+try {
+  const firebaseConfig = {
+    apiKey: "AIzaSyCDZfL4gBogSrPIMgpTwAclQhTixMyNf8o",
+    authDomain: "susiyaki-portfolio.firebaseapp.com",
+    projectId: "susiyaki-portfolio",
+    storageBucket: "susiyaki-portfolio.appspot.com",
+    messagingSenderId: "52419541464",
+    appId: "1:52419541464:web:5f4fab7f0f74106a98daad",
+    measurementId: "G-Z3EXZ0CJMS",
+  };
+  firebase.initializeApp(firebaseConfig);
+  db = firebase.firestore();
+} catch (error) {
+  console.log(error);
+}
 
-console.log(firebase);
+export { db };
