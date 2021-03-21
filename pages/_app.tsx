927b7da3959f87bfae76106afd8ce0156e23c5ad
susiyaki <susiyaki.dev@gmail.com>
2021-03-21 16:27:52 +0900

    add _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,12 @@
+import "twin.macro";
 import "./index.css";
 
 const App = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <div tw="h-screen w-screen">
+      <Component {...pageProps} />
+    </div>
+  );
 };
 
 export default App;
