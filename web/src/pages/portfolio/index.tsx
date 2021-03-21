8a53447be7c13626d482b543c4f2cb50c5c0bd9d
susiyaki <susiyaki.dev@gmail.com>
2021-03-21 17:38:12 +0900

    remove code

--- web/src/pages/portfolio/index.tsx
+++ web/src/pages/portfolio/index.tsx
@@ -1,14 +1,6 @@
 import "twin.macro";
-import { useEffect } from "react";
-import { careerDB } from "src/db/career";
 
 const Portfolio = () => {
-  useEffect(() => {
-    careerDB.get().then((res) => {
-      console.log(res);
-    });
-  }, []);
-
   return (
     <div tw="bg-yellow-50">
       <p>susiyaki portfolio</p>
